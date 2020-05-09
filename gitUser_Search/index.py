07fde9c4d1201dee3b633166f2bc6902b4300b18
Gabriel Davi <gdavi.gd@gmail.com>
2020-05-09 20:34:42 -0300

    wrapping up git search

--- gitUser_Search/index.py
+++ gitUser_Search/index.py
@@ -5,4 +5,8 @@ git_user = input("type your git username: ")
 reponse = requests.get('https://api.github.com/users/' + git_user)
 git = reponse.json()
 
-print(git))
+username = git['login']; 
+repos = git['public_repos']
+
+print("Your git username: " + username)
+print("The number of repos you have " + str(repos))
